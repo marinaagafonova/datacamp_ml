81aeab624adff3a6297043d0c2a602da7b817ccc
Marina Agafonova <agafonova_marina@yahoo.com>
2022-03-01 16:28:27 +0300

    Create 1-Common_data_problems.py

---
+++ Cleaning Data in Python/1-Common_data_problems.py
@@ -0,0 +1,83 @@
+# ---------- Exercise "Numeric data or ... ?" ----------
+
+# Print the information of ride_sharing
+print(ride_sharing.info())
+
+# Print summary statistics of user_type column
+print(ride_sharing['user_type'].describe())
+
+
+# ---------- Exercise "Summing strings and concatenating numbers" ----------
+
+# Strip duration of minutes
+ride_sharing['duration_trim'] = ride_sharing['duration'].str.strip('minutes')
+
+# Convert duration to integer
+ride_sharing['duration_time'] = ride_sharing['duration_trim'].astype('int')
+
+# Write an assert statement making sure of conversion
+assert ride_sharing['duration_time'].dtype == 'int'
+
+# Print formed columns and calculate average ride duration 
+print(ride_sharing[['duration','duration_trim','duration_time']])
+print(ride_sharing['duration_time'].mean())
+
+
+# ---------- Exercise "Tire size constraints" ----------
+
+# Convert tire_sizes to integer
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('int')
+
+# Set all values above 27 to 27
+ride_sharing.loc[ride_sharing['tire_sizes'] > 27, 'tire_sizes'] = 27
+
+# Reconvert tire_sizes back to categorical
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('category')
+
+# Print tire size description
+print(ride_sharing['tire_sizes'].describe())
+
+# ---------- Exercise "Back to the future" ----------
+
+# Convert ride_date to datetime
+ride_sharing['ride_dt'] = pd.to_datetime(ride_sharing['ride_date'])
+
+# Save today's date
+today = dt.date.today()
+
+# Set all in the future to today's date
+ride_sharing.loc[ride_sharing['ride_dt'] > today, 'ride_dt'] = today
+
+# Print maximum of ride_dt column
+print(ride_sharing['ride_dt'].max())
+
+
+# ---------- Exercise "Finding duplicates" ----------
+
+# Find duplicates
+duplicates = ride_sharing.duplicated('ride_id', keep=False)
+
+# Sort your duplicated rides
+duplicated_rides = ride_sharing[duplicates].sort_values('ride_id')
+
+# Print relevant columns of duplicated_rides
+print(duplicated_rides[['ride_id','duration','user_birth_year']])
+
+
+# ---------- Exercise "Treating duplicates" ----------
+
+# Drop complete duplicates from ride_sharing
+ride_dup = ride_sharing.drop_duplicates()
+
+# Create statistics dictionary for aggregation function
+statistics = {'user_birth_year': 'min', 'duration': 'mean'}
+
+# Group by ride_id and compute new statistics
+ride_unique = ride_dup.groupby('ride_id').agg(statistics).reset_index()
+
+# Find duplicated values again
+duplicates = ride_unique.duplicated(subset = 'ride_id', keep = False)
+duplicated_rides = ride_unique[duplicates == True]
+
+# Assert duplicates are processed
+assert duplicated_rides.shape[0] == 0
